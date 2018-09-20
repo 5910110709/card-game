7bbea4c9dabed021037ee82cd61c00111ad5214c
5910110709 <5910110709@psu.ac.th>
2018-09-20 10:45:27 +0700

    Add jsx in "App.js"

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 
+const word = "Hello";
 class App extends Component {
-  render() {
-  return (
-    <div>
-   <CharacterCard value="h"/>
-   <CharacterCard value="i"/>
-    </div>
-  );
-  }
+ render() {
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
  }
+ </div>
+ );
+ }
+}
  export default App;
  
